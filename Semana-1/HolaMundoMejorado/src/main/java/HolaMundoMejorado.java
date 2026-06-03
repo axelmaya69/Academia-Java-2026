8347941c059c7dc5c76283a89da80847740cf2c5
axelmaya69 <axeldbmaya96@gmail.com>
2026-06-03 12:24:16 -0600

    Comentando codigo

--- Semana-1/HolaMundoMejorado/src/main/java/HolaMundoMejorado.java
+++ Semana-1/HolaMundoMejorado/src/main/java/HolaMundoMejorado.java
@@ -2,14 +2,17 @@ public class HolaMundoMejorado {
     public static void main(String[] args) {
 
         String nombre = "Axel Daniel";
+        //Asignando valores a primitivos
         int edad = 25;
         double altura = 1.67;
         boolean esActivo = true;
 
+        //Mensaje concatenado
         String mensaje1 = "Me llamo "+ nombre + ", tengo "+ edad+" años, mido "+
                 altura+" m y estoy " + (esActivo ? "activo" : "inactivo") +".";
         System.out.println(mensaje1);
 
+        //Mensaje con metodo .format
         String mensaje2 = String.format("Me llamo %s, tengo %d años, mido %.2f m y estoy %s.",
                 nombre, edad, altura, esActivo ? "activo" : "inactivo");
         System.out.println(mensaje2);
