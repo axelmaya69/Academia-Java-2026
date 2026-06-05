ed820c88eec5775647d99214c5a0170b461aec2a
axelmaya69 <axeldbmaya96@gmail.com>
2026-06-04 21:58:46 -0600

    Creando clase abstracta vehiculo, esta implementa la interfaz pero como es una clase abstracta aun no puede implementar sus metodos, solo sus clases hijas

---
+++ Semana-1/6 Herencia de Vehiculos/src/main/java/VehiculoElectrico.java
@@ -0,0 +1,25 @@
+
+public class VehiculoElectrico extends Auto{
+    private int autonomiaKm;
+
+    public VehiculoElectrico(String marca, String modelo, int anio,
+                             int numPuertas, int autonomiaKm) {
+        super(marca, modelo, anio, numPuertas);
+        this.autonomiaKm = autonomiaKm;
+    }
+    @Override
+    public String tipoVehiculo() {
+        return "Vehiculo Electrico"; }
+
+    @Override
+    public void arrancar() {
+        System.out.println("  El vehiculo electrico " + marca
+                + " " + modelo + " arranca silenciosamente... "
+                + "(autonomia: " + autonomiaKm + " km)");
+    }
+
+    @Override
+    public String toString() {
+        return info() + " - autonomia " + autonomiaKm + " km";
+    }
+}
