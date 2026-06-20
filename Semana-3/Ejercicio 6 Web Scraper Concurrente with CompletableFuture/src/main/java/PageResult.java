6c350436c8b96c41a01d9097e866626dceda9e47
axelmaya69 <axeldbmaya96@gmail.com>
2026-06-20 14:28:29 -0600

    Creando record con sus atributos y un metodo toString

---
+++ Semana-3/Ejercicio 6 Web Scraper Concurrente with CompletableFuture/src/main/java/PageResult.java
@@ -0,0 +1,7 @@
+public record PageResult(String url, int statusCode, String title, long responseTimeMs) {
+
+    @Override
+    public String toString() {
+        return String.format("[%d] %s (%dms)", statusCode, title, responseTimeMs);
+    }
+}
